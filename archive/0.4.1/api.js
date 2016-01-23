5be6865d91f9343fb69bb97cec7b57b88b245368
Brian P. Hyder <brianhyder@gmail.com>
2016-01-23 10:27:25 -0500

    0.5.0

---
+++ archive/0.4.1/api.js
@@ -0,0 +1,177 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AdminNavigation",
+        "AdminSubnavService",
+        "AnalyticsManager",
+        "ApiActionController",
+        "ArticleRenderer",
+        "ArticleService",
+        "ArticleServiceV2",
+        "AsyncJobRunner",
+        "BaseApiController",
+        "BaseBodyParser",
+        "BaseController",
+        "BaseMediaRenderer",
+        "BaseObjectService",
+        "CacheEntityService",
+        "CacheFactory",
+        "CacheLockProvider",
+        "CallHomeService",
+        "ClientJs",
+        "ClusterJobRunner",
+        "CommandService",
+        "CommentService",
+        "Configuration",
+        "ContentObjectService",
+        "ContentService",
+        "ContentViewLoader",
+        "CustomObjectService",
+        "DAO",
+        "DBManager",
+        "DailyMotionMediaRenderer",
+        "DbEntityService",
+        "DbLockProvider",
+        "DeleteController",
+        "DocumentCreator",
+        "EmailService",
+        "ErrorFormatters",
+        "ErrorsOverTime",
+        "FSEntityService",
+        "FormAuthentication",
+        "FormBodyParser",
+        "FormController",
+        "FsMediaProvider",
+        "ImageMediaRenderer",
+        "InstagramMediaRenderer",
+        "JSONFSEntityService",
+        "JobRunner",
+        "JobService",
+        "JsonBodyParser",
+        "KickStarterMediaRenderer",
+        "LibrariesService",
+        "Localization",
+        "LockService",
+        "MediaLoader",
+        "MediaService",
+        "MemoryEntityService",
+        "MongoCommandBroker",
+        "MongoMediaProvider",
+        "MongoRegistrationProvider",
+        "MongoSessionStore",
+        "PB",
+        "PBError",
+        "PageRenderer",
+        "PageService",
+        "PencilBlue",
+        "PluginService",
+        "ReadOnlySimpleLayeredService",
+        "RedisCommandBroker",
+        "RedisRegistrationProvider",
+        "RedisSessionStore",
+        "RequestHandler",
+        "SectionService",
+        "SecurityService",
+        "ServerRegistration",
+        "SessionHandler",
+        "SettingsServiceFactory",
+        "SimpleLayeredService",
+        "SlideShareMediaRenderer",
+        "StorifyMediaRenderer",
+        "System",
+        "TTLIndexHelper",
+        "TemplateEntityService",
+        "TemplateService",
+        "TemplateValue",
+        "TopMenuService",
+        "TopicService",
+        "TrinketMediaRenderer",
+        "UrlService",
+        "UserService",
+        "UsernamePasswordAuthentication",
+        "Util",
+        "ValidationService",
+        "VideoMediaRenderer",
+        "View Controller",
+        "VimeoMediaRenderer",
+        "VineMediaRenderer",
+        "YouTubeMediaRenderer"
+    ],
+    "modules": [
+        "Admin",
+        "Database",
+        "Entities",
+        "ErrorSuccess",
+        "Model",
+        "Security",
+        "Services",
+        "Session",
+        "Storage",
+        "Theme",
+        "Validation",
+        "dao"
+    ],
+    "allModules": [
+        {
+            "displayName": "Admin",
+            "name": "Admin",
+            "description": "Provides function to construct the structure needed to display the navigation\nin the Admin section of the application."
+        },
+        {
+            "displayName": "dao",
+            "name": "dao",
+            "description": "Creates the cache"
+        },
+        {
+            "displayName": "Database",
+            "name": "Database",
+            "description": "Controlls the data model"
+        },
+        {
+            "displayName": "Entities",
+            "name": "Entities",
+            "description": "Provides information on media"
+        },
+        {
+            "displayName": "ErrorSuccess",
+            "name": "ErrorSuccess",
+            "description": "Specialized application error that knows what status code to return"
+        },
+        {
+            "displayName": "Model",
+            "name": "Model",
+            "description": "Creates structures for persistence and cleans various fields."
+        },
+        {
+            "displayName": "Security",
+            "name": "Security",
+            "description": "Service for managing user access"
+        },
+        {
+            "displayName": "Services",
+            "name": "Services",
+            "description": "Service for creating JavaScript tags"
+        },
+        {
+            "displayName": "Session",
+            "name": "Session",
+            "description": "Session storage backed by MongoDB"
+        },
+        {
+            "displayName": "Storage",
+            "name": "Storage",
+            "description": "Services for managing storage"
+        },
+        {
+            "displayName": "Theme",
+            "name": "Theme",
+            "description": "Theme content services"
+        },
+        {
+            "displayName": "Validation",
+            "name": "Validation",
+            "description": "Provides a set of functions for common validations."
+        }
+    ]
+} };
+});
